fe7ddda4379f488a29946b7fc682fa547a5d73b6
Eric Brandwein <brandweineric@gmail.com>
2019-04-17 18:07:01 -0300

    Script para loguear paquetes ARP

---
+++ tp1/sniffear_arp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from scapy.all import sniff, ARP
+
+
+def op_to_string(operation):
+    return 'who-has' if operation == 1 else 'is-at'
+
+
+def entropy_callback(paquete):
+    try:
+        print(
+            ','.join([
+                'ARP',
+                op_to_string(paquete[ARP].op),
+                paquete[ARP].psrc,
+                paquete[ARP].pdst,
+            ])
+        )
+    except:
+        pass
+
+
+print('method,operation,source,destination')
+sniff(prn=entropy_callback, filter="arp")
