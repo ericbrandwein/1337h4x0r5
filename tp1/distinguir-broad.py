8f63f9beaa86524a4b5e2063f8696baa32e7afe1
Eric Brandwein <brandweineric@gmail.com>
2019-04-27 16:09:14 -0300

    Pasa a python3 distinguir_broad.py
    Y de paso lo limpia un poquito.

--- tp1/distinguir-broad.py
+++ tp1/distinguir-broad.py
@@ -1,55 +1,54 @@
-
-import sys, os
+import sys
+import os
 from math import log as LOG
 from scapy.all import *
 
 i = 0
+
+
 def i_pack():
-	global i
-	i +=1
-	print "paquete nro : ", i
-	os.system("clear")		
-	print "paquete nro : ", i
-	os.system("clear")		
-	print "paquete nro : ", i
-	os.system("clear")		
-	print "paquete nro : ", i
-	
+    global i
+    i += 1
+    os.system("clear")
+    print("paquete nro : ", i)
+
 
 def mostrarNodos(source):
-	H = 0
-	N = sum(source.values())
+    H = 0
+    N = sum(source.values())
 
-	nodes = []
-	for a, c in source.iteritems():
-		p = c/float(N)
-		i = -LOG(p, 2)
-		H += p * i
-		nodes.append((a,p))
+    nodes = []
+    for a, c in source.iteritems():
+        p = c/float(N)
+        i = -LOG(p, 2)
+        H += p * i
+        nodes.append((a, p))
 
-	# nodes.sort(key=lambda n: n[1])
-	print "Entropia", H, "Entropia maxima", LOG(len(nodes),2)
-	for a,p in nodes:
-		print a , " " , ("%.5f" % p) 
+    # nodes.sort(key=lambda n: n[1])
+    print("Entropia", H, "Entropia maxima", LOG(len(nodes), 2))
+    for a, p in nodes:
+        print(a, " ", ("%.5f" % p))
+
 
 def trafico(pkt):
-	i_pack()
-	try:
-		if pkt.dst == 'ff:ff:ff:ff:ff:ff': # es broadcast
-			if ('bcast',hex(pkt.type)) not in frec:
-				frec[('bcast',hex(pkt.type))] = 0
-			frec[('bcast',hex(pkt.type))] += 1
-		else:				  # es unicast (o quiza multicast)	
-			if ('unicast',hex(pkt.type)) not in frec:
-				frec[('unicast',hex(pkt.type))] = 0
-			frec[('unicast',hex(pkt.type))] += 1
-	except:	
-		 return
+    i_pack()
+    try:
+        if pkt.dst == 'ff:ff:ff:ff:ff:ff':  # es broadcast
+            pkt_dest_type = 'bcast'
+        else:  # es unicast (o quiza multicast)
+            pkt_dest_type = 'unicast'
+
+        if (pkt_dest_type, hex(pkt.type)) not in frec:
+            frec[(pkt_dest_type, hex(pkt.type))] = 0
+        frec[(pkt_dest_type, hex(pkt.type))] += 1
+
+    except:
+        return
 
 
-frec = {}	
+frec = {}
 
 sniff(prn=trafico, count=12000)
-print "mi fuente"
-print frec.items() , "\n"
+print("mi fuente")
+print(frec.items(), "\n")
 mostrarNodos(frec)
