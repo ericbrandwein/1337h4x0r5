0b2353fc8b92ab2caab24c0329968ed0e487f7f3
nsanmartin <smnicolas>
2019-04-28 16:14:53 -0300

    retoco el graficador

---
+++ tp1/R/graficos.R
@@ -0,0 +1,26 @@
+
+grafico_barras0.s1 <- function(info.src) {
+    stopifnot("info.src" %in% class(info.src))
+    entr <- entropia(info.src)
+    entr.max <- entropia_max(info.src)
+    
+    ggplot(data = info.src, aes(x=symbols, y=info)) +
+        geom_bar (stat = "identity") +
+        geom_hline(yintercept = entr, color = "blue") +
+        geom_hline(yintercept = entr.max, color   = "red") +
+        annotate ("text", 0.75, entr * 1.1 , label = "entropía") +
+        annotate ("text", 1, entr.max * 1.1 , label = "entropía máxima") +
+
+        ggtitle("Información de cada símbolo")
+}
+
+
+
+grafico_barras.s1 <- function(info.src) {
+    stopifnot("info.src" %in% class(info.src))
+    ggplot(data = info.src, aes(x=symbols, y=info)) +
+        geom_bar (stat = "identity") +
+        geom_hline(aes(yintercept = entropia(info.src), colour = "entropía")) +
+        geom_hline(aes(yintercept = entropia_max(info.src), colour = "entropía\nmáxima")) +
+        ggtitle("Información de cada símbolo")
+}
