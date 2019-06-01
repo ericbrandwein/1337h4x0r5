f19e4bb80f63f6ebcc63d8e892c005440428245d
Eric Brandwein <brandweineric@gmail.com>
2019-06-01 20:14:04 -0300

    [tp2] Traceroute con rtt

--- tp2/py/traceroute.py
+++ tp2/py/traceroute.py
@@ -2,41 +2,56 @@
 
 from scapy.all import *
 import sys
-
+import os
+import time
+import csv
 
 def is_ICMP_echo_reply (answer):
     return len(answer.res) > 0 and answer.res[0][1].type == 0
 
 def get_ICMP_error_src (answer):
-    return len(answer.res) > 0 and answer.res[0][1].src
+    return 'NA' if len(answer.res) == 0 else answer.res[0][1].src
 
     
-def get_hops(ip_addr):
+def write_hops(ip_addr, csv_writer):
     hops = []
-    
-    ttl = 1
     cota = 32
-    for times in range(cota):
-        ans, unans = sr(IP(dst = ip_addr, ttl = ttl) / ICMP(), timeout=1)
+    for ttl in range(1, cota):
+        for packets_sent in range(32):
+            start = time.time()
 
-        if is_ICMP_echo_reply (ans): break
-        else:
-            hops.append (get_ICMP_error_src(ans))
-            ttl = ttl + 1
+            ans, unans = sr(IP(dst = ip_addr, ttl = ttl) / ICMP(), timeout=1)
+            
+            end = time.time()
+            if is_ICMP_echo_reply(ans):
+                break
+
+            if unans:
+                elapsed_time = 'NA'
+            else:
+                elapsed_time = end - start
+
+            csv_writer.writerow([get_ICMP_error_src(ans), elapsed_time, ttl])
+
 
     print("----------------------------------------------------------------------------------------")
     return hops
 
-def main(ip_addr):
-    hops = get_hops(ip_addr)
-    for i, hop in enumerate(hops):
-        print("{}\t{}".format(i, hop))
+def main(ip_addr, csv_file):
+    exists = os.path.isfile(csv_file)
 
-
-              
+    if exists:
+        print("El archivo ya existe")
+    else:
+        bufsize = 1
+        with open(csv_file, 'w', bufsize) as archivo:
+            writer = csv.writer(archivo)
+            writer.writerow(['dst', 'rtt', 'ttl'])
+            write_hops(ip_addr, writer)
+            
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("tenes que darme una direccion de ip")
+    if len(sys.argv) < 3:
+        print("tenes que darme una direccion de ip y un archivo csv")
     else:
-        main(sys.argv[1])
+        main(sys.argv[1], sys.argv[2])
